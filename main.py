1c9fa178c92c203ec30ead4adefa4a3cacaa7e14
CarlosR759 <80783790+CarlosR759@users.noreply.github.com>
2022-11-05 13:04:49 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import logging
 from modelos.lectura import lectura_instancia_del_problema
-
+from modelos.tabuSearch import tabu_search
 
 def resolver_scp():
-    func = lectura_instancia_del_problema() 
-    print(func)
+    problema = lectura_instancia_del_problema() 
+    solucion = tabu_search(problema)
+    #return solucion
+
 
 #MAIN#
 resolver_scp()
-
+#print(solucion)
 
