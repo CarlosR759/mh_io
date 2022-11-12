cd7a1695170664ced3bd4d38e269b102bcc4abcf
CarlosR759 <80783790+CarlosR759@users.noreply.github.com>
2022-11-12 20:39:20 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,10 +6,13 @@ from modelos.fitness import fitness
 def resolver_scp():
     problema = lectura_instancia_del_problema() 
     solucion = tabu_search(problema)
+    print("solucion encontrada: ")
+    print(solucion)
+    print("Con fitnes igual a : ")
+    fitness_solucion = fitness(problema[0], solucion)
+    print(fitness_solucion)
     return solucion
+
 
 #MAIN#
 solucion_final = resolver_scp()
-print("solucion encontrada: ")
-print(solucion_final)
-print("Con fitnes igual a : ")
